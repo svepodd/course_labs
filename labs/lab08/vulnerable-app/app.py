73ee911b56219d5c9fa565248134f8663e053b2e
Svetlana Poddoskina <91290418+svepodd@users.noreply.github.com>
2026-05-24 14:52:45 +0300

    Add nosec comments
    Added nosec comments to suppress security warnings.

--- labs/lab08/vulnerable-app/app.py
+++ labs/lab08/vulnerable-app/app.py
@@ -261,4 +261,4 @@ def files(subpath=""):
 
 if __name__ == "__main__":
     init_db()
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)  # nosec B104 B201
